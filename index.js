0030194c1ca8451add605608a4df20966de4415d
Philip Heindel <p.e.heindel@gmail.com>
2020-03-26 17:28:14 -0400

    Added cors support

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const server = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const uuid = require("uuid");
+
+server.use(cors());
+server.use(bodyParser.json());
 
 server.get("/", (req, res) => {
   let id = uuid.v4();
