1834b12b24debf5b29f491708d47119687fb8412
Philip Heindel <p.e.heindel@gmail.com>
2020-03-26 17:01:03 -0400

    Added uuid test output to get /

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const server = express();
 const port = process.env.PORT || 3000;
+const uuid = require("uuid");
 
-var app = express();
-
-app.get("/", (req, res) => {
-  res.send(JSON.stringify({ Hello: "World" }));
+server.get("/", (req, res) => {
+  let id = uuid.v4();
+  console.log("sending uuid:" + id);
+  res.status(200).json({
+    id: id
+  });
 });
 
-app.listen(port, () => {
-  console.log("Example app listening on port !");
+server.listen(port, () => {
+  console.log("Example app listening on port " + port + "!");
 });
